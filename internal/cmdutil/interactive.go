29b364b783624f8474344e533e46cb83ef4ed556
estebanmilaho <stephan.mirlach@gmail.com>
2026-02-06 09:55:11 +0100

    Improve robustness

--- internal/cmdutil/interactive.go
+++ internal/cmdutil/interactive.go
@@ -1,8 +1,7 @@
 package cmdutil
 
 import (
-	"bufio"
-	"fmt"
+	"bytes"
 	"io"
 	"os"
 	"os/exec"
@@ -15,37 +14,19 @@ func ChooseWithFzf(items []string, header string) (string, error) {
 		args = append(args, "--header", header)
 	}
 	cmd := exec.Command("fzf", args...)
-	in, err := cmd.StdinPipe()
-	if err != nil {
-		return "", err
-	}
-	out, err := cmd.StdoutPipe()
-	if err != nil {
-		return "", err
-	}
 	cmd.Stderr = os.Stderr
+	cmd.Stdin = strings.NewReader(strings.Join(items, "\n") + "\n")
 
-	if err := cmd.Start(); err != nil {
-		return "", err
-	}
-
-	go func() {
-		w := bufio.NewWriter(in)
-		for _, it := range items {
-			fmt.Fprintln(w, it)
-		}
-		w.Flush()
-		in.Close()
-	}()
-
-	choiceBytes, err := io.ReadAll(out)
-	if err != nil {
-		return "", err
-	}
-
-	if err := cmd.Wait(); err != nil {
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	if err := cmd.Run(); err != nil {
 		// fzf returns non-zero on cancel
 		return "", nil
+	}
+
+	choiceBytes, err := io.ReadAll(&stdout)
+	if err != nil {
+		return "", err
 	}
 
 	return strings.TrimSpace(string(choiceBytes)), nil
